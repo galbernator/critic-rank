bfa39858272474c8e793796bea8af9ab97721db4
Steve Galbraith <steve@stevegalbraith.com>
2015-08-28 23:40:52 -0600

    Added ability to access Facebook user movie list

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
+  var facebookID
   $('#get-it').click(function(){
     OAuth.popup('facebook', function(error, result) {
+      var accessToken = result.access_token;
+      // get the user ID to be able to search the movies on their Facebook page
       if (error) {
         alert(error); // do something with error
         return;
       } else {
-        result.get('/me').done(function (response) {
-            //this will display "John Doe" in the console
-            console.log(response);
-            for (var i = 0; i < response.length; i += 1) {
-              var info = response[i];
-              console.log(info);
-            }
+        result.me().done(function (response) {
+          facebookID = response.id;
+          var url = 'https://graph.facebook.com/v2.3/' + facebookID + '/movies?access_token=' + accessToken
+          console.log(response);
+          $.getJSON(url, function(data) {
+            console.log(data);
+            // loop through each of the results and then call addProduct on each result
+            for (var i=0; i < data.data.length; i += 1) {
+              var movie = data.data[i].name;
+              console.log(movie);
+            //   addWalmartProduct(company, movie);
+            };
+          })
         })
         .fail(function (err) {
             //handle error with err
         });
       }
+
+      // Use the ID to get the movies
+
+
     });
   });
 });
