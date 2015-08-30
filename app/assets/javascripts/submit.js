e8d48fee461fcda48e55004493cba4e9aa4a893e
Steve Galbraith <steve@stevegalbraith.com>
2015-08-29 20:23:52 -0600

    Finalized calls to Facebook API for movies and return title and year

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+
   var facebookID
+
+  var changeButton = function() {
+    $('#button').html('')
+    $('#button').html('<button id="share-it" class="btn btn-lg btn-warning">Share Your Rating</button>')
+  };
+
   $('#get-it').click(function(){
     OAuth.popup('facebook', function(error, result) {
       var accessToken = result.access_token;
@@ -11,17 +18,41 @@ $(document).ready(function() {
         result.me().done(function (response) {
           // Use the ID to get the movies
           facebookID = response.id;
-          var url = 'https://graph.facebook.com/v2.3/' + facebookID + '/movies?access_token=' + accessToken
-          console.log(response);
-          $.getJSON(url, function(data) {
-            console.log(data);
-            // loop through each of the results and then call addProduct on each result
-            for (var i=0; i < data.data.length; i += 1) {
-              var movie = data.data[i].name;
-              console.log(movie);
-            //   addWalmartProduct(company, movie);
-            };
-          })
+          var userUrl = 'https://graph.facebook.com/v2.3/' + facebookID + '/movies?access_token=' +
+                         accessToken
+          var likedMovies
+          changeButton();
+
+          $.ajax({
+            url: userUrl,
+            dataType: 'json',
+            success: function(data) {
+                      likedMovies = data.data
+                    },
+            complete: function() {
+
+                        $.each(likedMovies, function(i, movie) {
+                          var movieID = movie.id;
+                          var movieTitle = movie.name;
+                          var releaseDate
+                          var movieUrl = 'https://graph.facebook.com/v2.3/' + movieID +
+                                         '?fields=release_date&access_token=' + accessToken
+                          $.ajax({
+                            url: movieUrl,
+                            dataType: 'json',
+                            success: function(data) {
+                              console.log(data);
+                              releaseDate = data.release_date.substring(data.release_date.length - 4);
+                              console.log(movieTitle + ' ' + releaseDate);
+                            }
+                          })
+                        });
+                        // loop through each movie
+                        // for (var i=0; i < likedMovies.length; i += 1) {
+                        //   console.log(movieTitle);
+                        // }
+                      }
+            });
         })
         .fail(function (err) {
             //handle error with err
